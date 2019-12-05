7505e0a94df11c249a71964c722eaa81fc0c903b
Hugo Nogueira <hugomn@gmail.com>
2019-12-05 16:01:15 +0500

    Remove node_modules folder

--- packages/foundation/src/themes/default.ts
+++ packages/foundation/src/themes/default.ts
@@ -1,7 +1,38 @@
 import Foundation from "..";
 
 const DefaultTheme: Foundation = {
+  color: {
+    // Primary colors
+    // primaryDarker: string;
+    // primaryDark: string;
+    primary: "#5E77FF",
+    // primaryLight: string;
+    // primaryLighter: string;
+    // primaryLightest: string;
 
+    // Secondary colors
+    // secondaryDarker: string;
+    // secondaryDark: string;
+    secondary: "#7BC3BD",
+    // secondaryLight: string;
+    // secondaryLighter: string;
+    // secondaryLightest: string;
+
+    // Black and grays
+    black: "#121212",
+    // grayDarker: string;  
+    // grayDark: string;
+    gray: "#454545",
+    // grayLight: string;
+    // grayLighter: string;
+    // grayLightest: string;
+
+    // Statuses colors
+    statusError: "#f44336",
+    statusInfo: "#3782b0",
+    statusSuccess: "#46a26d",
+    statusWarning: "#c39135"
+  }
 }
 
 export default DefaultTheme;
